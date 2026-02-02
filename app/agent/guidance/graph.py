d7dcae77282b392565e403c7d2b1b490ee6e360c
jongmai <whdghksdl906@gmail.com>
2026-02-02 14:36:42 +0900

    guidance 비동기함수 오류 수정

--- app/agent/guidance/graph.py
+++ app/agent/guidance/graph.py
@@ -4,7 +4,7 @@ from app.agent.guidance.state import AgentState
 from app.agent.guidance.nodes import analyze_messages_node, retrieval_node, generate_node
 
 # conditional edge
-async def decide_rag(state: AgentState):
+def decide_rag(state: AgentState):
   """ 상담 내역 분석 후 Agent가 어떤 행동을 할지 라우팅하는 함수
   1. 분석 결과 "retrieve": RAG 활용해 추천 답변과 다음 행동 제시
   2. 분석 결과 "generate": RAG 스킵하고 추천 답변과 다음 행동 제시
